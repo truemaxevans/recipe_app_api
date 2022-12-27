e973079be7eac508c049dff1611f62d1568b564a
truemaxevans <daorodney@gmail.com>
2022-12-27 23:51:20 +0200

    fixing using flake8

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,15 +3,11 @@ from app import calc
 
 
 class SimpleTests(SimpleTestCase):
-    
-        def test_calc(self):
-            res = calc.add(1, 2)
-            self.assertEqual(res, 3)
-        
-        
-        def test_subtract(self):
-            res = calc.subtract(15, 10)
-            self.assertEqual(res, 5)
 
-        
+    def test_calc(self):
+        res = calc.add(1, 2)
+        self.assertEqual(res, 3)
 
+    def test_subtract(self):
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
